f9cd723245ad22e7940d3af749fbfef7798ae5a4
Rodrigo Chamun <rochamun@gmail.com>
2014-04-23 18:54:11 -0300

    Add View and ViewAdapter

---
+++ Processing Utils/src/view/View.java
@@ -0,0 +1,10 @@
+package view;
+
+public interface View {
+	public void mousePressed();
+	public void mouseDragged();
+	public void mouseReleased();
+	public void mouseClicked();
+	public void keyPressed();
+	public void draw();
+}
---
+++ Processing Utils/src/view/ViewAdapter.java
@@ -0,0 +1,23 @@
+package view;
+
+public abstract class ViewAdapter implements View {
+
+	@Override
+	public void mousePressed() { }
+
+	@Override
+	public void mouseDragged() { }
+
+	@Override
+	public void mouseReleased() { }
+
+	@Override
+	public void mouseClicked() { }
+
+	@Override
+	public void keyPressed() { }
+
+	@Override
+	public void draw() { }
+
+}
