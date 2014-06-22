4ca364edb930b0e38796b13227d31f09a6fe3ab2
Rodrigo Chamun <rochamun@gmail.com>
2014-06-22 16:24:51 -0300

    Mode is dynamic

--- Processing Utils/src/gui/Modes.java
+++ Processing Utils/src/gui/Modes.java
@@ -7,8 +7,9 @@ public class Modes implements PanelItem {
 	
 	private String title;
 	private List<String> modes;
-	private String currentContent;
+	private List<String> keys = new ArrayList<String>();
 	private int currentMode;
+	private boolean usingKeys = false;
 
 	public Modes(String title, String defaultMode) {
 		this.title = title;		
@@ -18,16 +19,49 @@ public class Modes implements PanelItem {
 		changeMode(0);
 	}
 	
+	public Modes(String title, String defaultMode, String key) {
+		this(title, defaultMode);
+		keys.add(key);
+		usingKeys = true;
+	}
+	
 	public void addMode(String newMode) { modes.add(newMode); }
+	public void addMode(String newMode, String key) {
+		modes.add(newMode);
+		keys.add(key);
+		usingKeys = true;
+	}
 	
 	public void changeMode(int newMode) {
 		if (newMode >= modes.size() || newMode < 0)
 			throw new RuntimeException("Invalid mode!");
-		currentContent = title + " " + modes.get(newMode);
 		currentMode = newMode;
 	}
 	
+	private String getTitle() {
+		String ret = title;
+		if (keys.size() > 0)
+			ret += " " + getOptions();
+		if (usingKeys)
+			ret += ":";
+		return ret;
+	}
+
+	private String getOptions() {
+		StringBuffer sb = new StringBuffer();
+		int count = 1;
+		sb.append("(");
+		for (String s: keys) {
+			sb.append(s);
+			if (count < keys.size())
+				sb.append("/");
+			count++;
+		}
+		sb.append(")");
+		return sb.toString();
+	}
+
 	public int getCurrentMode() { return currentMode; }
-	public String getString() { return currentContent; }
+	public String getString() { return getTitle() + " " + modes.get(currentMode); }
 
 }
